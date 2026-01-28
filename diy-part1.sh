56d77cbd9d560414746d2c47af9259cf19e84fed
hanhai080 <hanhai080@gmail.com>
2026-01-28 09:42:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# 1. 开启 helloworld 插件源（如果源码自带则取消注释）
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 2. 强制换行并添加三个核心插件源 (使用 echo >> 确保每个源独占一行，防止 Username 报错)
+# 1. 强制在配置文件末尾先打一个回车，确保新内容不跟旧内容粘在一起
 echo >> feeds.conf.default
+
+# 2. 用 echo 方式添加插件源，这会自动处理换行，最稳妥
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
