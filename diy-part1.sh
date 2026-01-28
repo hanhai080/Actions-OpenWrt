1da5c33337edf48db2fcddc59e9a4193a65a344f
hanhai080 <hanhai080@gmail.com>
2026-01-28 09:33:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,20 +4,12 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 开启 helloworld 插件源（如果源码自带则取消注释）
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 添加 helloworld 插件源
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-
-# 添加 Lucky 插件源 (核心需求)
-sed -i '$a src-git lucky https://github.com/gdy666/luci-app-lucky.git' feeds.conf.default
-
-# 添加 PassWall 插件源 (核心需求)
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+# 2. 强制换行并添加三个核心插件源 (使用 echo >> 确保每个源独占一行，防止 Username 报错)
+echo >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
