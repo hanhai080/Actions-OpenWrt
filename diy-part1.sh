27080c8a481dbb8c9cda9c70efe4bc9f525ac195
hanhai080 <hanhai080@gmail.com>
2026-01-28 09:51:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,7 @@
-# 1. 强制在配置文件末尾先打一个回车，确保新内容不跟旧内容粘在一起
+# 强制换行并清理可能存在的旧源
 echo >> feeds.conf.default
 
-# 2. 用 echo 方式添加插件源，这会自动处理换行，最稳妥
+# 使用更稳定的镜像/备用地址
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
+echo 'src-git passwall https://github.com/chenmozhijia/openwrt-passwall.git' >> feeds.conf.default
